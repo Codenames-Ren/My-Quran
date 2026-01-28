026efc872301739c41dc31a29da7fadefb25767e
Codenames-Ren <renmizuki38@gmail.com>
2026-01-28 22:12:55 +0700

    refactor structure and updating audio for all surah

--- app/quran.tsx
+++ app/quran.tsx
@@ -1,45 +1,20 @@
+import { QuranList } from "@/components/quran/QuranList";
 import { getAllSurah } from "@/src/api/surat";
-import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function QuranScreen() {
   const [surah, setSurah] = useState<any[]>([]);
 
   useEffect(() => {
     getAllSurah()
-      .then((res: any) => setSurah(res.data))
+      .then((res) => setSurah(res.data))
       .catch(console.error);
   }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: "#0F172A" }}>
-      <FlatList
-        data={surah}
-        keyExtractor={(item) => item.nomor.toString()}
-        renderItem={({ item }) => (
-          <Link
-            href={{
-              pathname: "/surah/[id]",
-              params: { id: item.nomor.toString() },
-            }}
-            asChild
-          >
-            <Pressable>
-              <Text
-                style={{
-                  color: "white",
-                  padding: 16,
-                  borderBottomWidth: 0.5,
-                  borderBottomColor: "#334155",
-                }}
-              >
-                {item.nomor}, {item.namaLatin}
-              </Text>
-            </Pressable>
-          </Link>
-        )}
-      />
+      <QuranList surah={surah} />
     </View>
   );
 }
--- app/surah/[id].tsx
+++ app/surah/[id].tsx
@@ -1,28 +1,45 @@
+import { AyatItem } from "@/components/quran/AyatItem";
+import { SurahHeader } from "@/components/quran/SurahHeader";
 import { getSurahDetail } from "@/src/api/surat";
-import { toArabicNumber } from "@/src/utils/arabic";
+import { useAyatAudio } from "@/src/hooks/useAyatAudio";
+import { QoriKey, getNextQori } from "@/src/utils/qori";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SurahDetailScreen() {
   const { id } = useLocalSearchParams();
   const navigation = useNavigation();
-  const [surah, setSurah] = useState<any>(null);
   const insets = useSafeAreaInsets();
+
+  const [surah, setSurah] = useState<any>(null);
+  const [qori, setQori] = useState<QoriKey>("05");
+
+  const { playAyat, playingAyat, progress } = useAyatAudio();
 
   useEffect(() => {
     getSurahDetail(Number(id))
-      .then((res: any) => {
+      .then((res) => {
         setSurah(res.data);
-        navigation.setOptions({
-          title: res.data.namaLatin,
-        });
+        navigation.setOptions({ title: res.data.namaLatin });
       })
       .catch(console.error);
   }, [id]);
 
   if (!surah) return null;
+
+  function handlePlayAyat(ayatindex: number) {
+    const ayat = surah.ayat[ayatindex];
+
+    playAyat(ayat.audio[qori], ayat.nomorAyat, () => {
+      const nextIndex = ayatindex + 1;
+
+      if (nextIndex < surah.ayat.length) {
+        handlePlayAyat(nextIndex);
+      }
+    });
+  }
 
   return (
     <ScrollView
@@ -32,66 +49,8 @@ export default function SurahDetailScreen() {
         paddingBottom: Math.max(insets.bottom, 16) + 48,
       }}
     >
-      {/* Header */}
-      <View style={{ alignItems: "center", marginBottom: 32 }}>
-        <Text
-          style={{
-            color: "white",
-            fontSize: 32,
-            marginBottom: 4,
-            textAlign: "center",
-          }}
-        >
-          {surah.nama}
-        </Text>
-        {/* Nama Latin */}
-        <Text
-          style={{
-            color: "#C8D5E1",
-            fontSize: 18,
-            marginBottom: 10,
-          }}
-        >
-          {surah.namaLatin}
-        </Text>
-        {/* Info Surah */}
-        <View
-          style={{
-            flexDirection: "row",
-            gap: 8,
-            paddingVertical: 6,
-            paddingHorizontal: 10,
-            borderRadius: 20,
-            backgroundColor: "#1E2938",
-          }}
-        >
-          <Text
-            style={{
-              color: "#94A38B",
-              fontSize: 12,
-            }}
-          >
-            Diturunkan di {surah.tempatTurun}
-          </Text>
-          <Text
-            style={{
-              color: "#94A38B",
-              fontSize: 12,
-            }}
-          >
-            •
-          </Text>
-          <Text
-            style={{
-              color: "#94A38B",
-              fontSize: 12,
-            }}
-          >
-            {surah.jumlahAyat} Ayat
-          </Text>
-        </View>
-      </View>
-      {/* Basmalah */}
+      <SurahHeader surah={surah} />
+
       {surah.nomor !== 1 && surah.nomor !== 9 && (
         <Text
           style={{
@@ -99,55 +58,23 @@ export default function SurahDetailScreen() {
             fontSize: 28,
             textAlign: "center",
             marginBottom: 32,
-            lineHeight: 48,
             fontFamily: "Scheherazade",
           }}
         >
           بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
         </Text>
       )}
-      {/* Ayat */}
-      {surah.ayat.map((ayat: any) => (
-        <View
+
+      {surah.ayat.map((ayat: any, index: number) => (
+        <AyatItem
           key={ayat.nomorAyat}
-          style={{
-            marginBottom: 28,
-          }}
-        >
-          {/* Nomor Ayat */}
-          <Text
-            style={{
-              color: "white",
-              fontSize: 26,
-              lineHeight: 48,
-              textAlign: "right",
-              marginBottom: 10,
-              fontFamily: "Scheherazade",
-            }}
-          >
-            {ayat.teksArab} ۝{toArabicNumber(ayat.nomorAyat)}
-          </Text>
-          {/* Latin */}
-          <Text
-            style={{
-              color: "#94A3B8",
-              marginBottom: 10,
-              fontStyle: "italic",
-            }}
-          >
-            {ayat.teksLatin}
-          </Text>
-          {/* Terjemahan */}
-          <Text
-            style={{
-              color: "white",
-              lineHeight: 22,
-              marginBottom: 10,
-            }}
-          >
-            {ayat.nomorAyat}. {ayat.teksIndonesia}
-          </Text>
-        </View>
+          ayat={ayat}
+          isPlaying={playingAyat === ayat.nomorAyat}
+          progress={playingAyat === ayat.nomorAyat ? progress : 0}
+          qori={qori}
+          onPlay={() => handlePlayAyat(index)}
+          onNextQori={() => setQori(getNextQori(qori))}
+        />
       ))}
     </ScrollView>
   );
