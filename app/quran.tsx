b9ce70f62437dd67fa83fb26c0aae4fdd19a6259
Codenames-Ren <renmizuki38@gmail.com>
2026-01-29 21:36:49 +0700

    add search bar and highlighting the result

--- app/quran.tsx
+++ app/quran.tsx
@@ -1,10 +1,12 @@
-import { QuranList } from "@/components/quran/QuranList";
+import { QuranSearchController } from "@/components/quran/QuranSearchController";
 import { getAllSurah } from "@/src/api/surat";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function QuranScreen() {
   const [surah, setSurah] = useState<any[]>([]);
+  const insets = useSafeAreaInsets();
 
   useEffect(() => {
     getAllSurah()
@@ -13,8 +15,14 @@ export default function QuranScreen() {
   }, []);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#0F172A" }}>
-      <QuranList surah={surah} />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#0F172A",
+        paddingBottom: Math.max(insets.bottom, 16),
+      }}
+    >
+      <QuranSearchController surah={surah} />
     </View>
   );
 }
