092f5c458de918432abff13231accf6524bc210f
Codenames-Ren <renmizuki38@gmail.com>
2026-01-30 20:11:41 +0700

    updating surah screen for new search feature

--- app/surah/[id].tsx
+++ app/surah/[id].tsx
@@ -1,11 +1,12 @@
-import { AyatItem } from "@/components/quran/AyatItem";
-import { SurahHeader } from "@/components/quran/SurahHeader";
+import { AyatItem } from "@/components/quran/surah/AyatItem";
+import { AyatSearchController } from "@/components/quran/surah/AyatSearchController";
+import { SurahHeader } from "@/components/quran/surah/SurahHeader";
 import { getSurahDetail } from "@/src/api/surat";
 import { useAyatAudio } from "@/src/hooks/useAyatAudio";
 import { QoriKey, getNextQori } from "@/src/utils/qori";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SurahDetailScreen() {
@@ -15,8 +16,11 @@ export default function SurahDetailScreen() {
 
   const [surah, setSurah] = useState<any>(null);
   const [qori, setQori] = useState<QoriKey>("05");
+  const [highlight, setHighlightAyat] = useState<number | null>(null);
 
   const { playAyat, playingAyat, progress } = useAyatAudio();
+
+  const listRef = useRef<FlatList>(null);
 
   useEffect(() => {
     getSurahDetail(Number(id))
@@ -38,6 +42,17 @@ export default function SurahDetailScreen() {
     });
   }
 
+  function jumpToAyat(ayatNumber: number | null) {
+    if (!ayatNumber) return;
+
+    const index = surah.ayat.findIndex((a: any) => a.nomorAyat === ayatNumber);
+
+    if (index >= 0) {
+      listRef.current?.scrollToIndex({ index, animated: true });
+      setHighlightAyat(ayatNumber);
+    }
+  }
+
   return (
     <View
       style={{
@@ -46,38 +61,46 @@ export default function SurahDetailScreen() {
         paddingBottom: Math.max(insets.bottom, 16),
       }}
     >
-      <ScrollView
-        style={{ flex: 1, backgroundColor: "#0F172A" }}
+      <AyatSearchController
+        totalAyat={surah.jumlahAyat}
+        onJumpToAyat={jumpToAyat}
+      />
+
+      <FlatList
+        ref={listRef}
+        data={surah.ayat}
+        keyExtractor={(item) => item.nomorAyat.toString()}
         contentContainerStyle={{ padding: 16 }}
-      >
-        <SurahHeader surah={surah} />
-
-        {surah.nomor !== 1 && surah.nomor !== 9 && (
-          <Text
-            style={{
-              color: "white",
-              fontSize: 28,
-              textAlign: "center",
-              marginBottom: 32,
-              fontFamily: "Scheherazade",
-            }}
-          >
-            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
-          </Text>
-        )}
-
-        {surah.ayat.map((ayat: any, index: number) => (
+        ListHeaderComponent={
+          <>
+            <SurahHeader surah={surah} />
+            {surah.nomor !== 1 && surah.nomor !== 9 && (
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 28,
+                  textAlign: "center",
+                  marginBottom: 32,
+                  fontFamily: "Scheherazade",
+                }}
+              >
+                بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+              </Text>
+            )}
+          </>
+        }
+        renderItem={({ item, index }) => (
           <AyatItem
-            key={ayat.nomorAyat}
-            ayat={ayat}
-            isPlaying={playingAyat === ayat.nomorAyat}
-            progress={playingAyat === ayat.nomorAyat ? progress : 0}
+            ayat={item}
+            isPlaying={playingAyat === item.nomorAyat}
+            progress={playingAyat === item.nomorAyat ? progress : 0}
             qori={qori}
+            highlighted={highlight === item.nomorAyat}
             onPlay={() => handlePlayAyat(index)}
             onNextQori={() => setQori(getNextQori(qori))}
           />
-        ))}
-      </ScrollView>
+        )}
+      ></FlatList>
     </View>
   );
 }
