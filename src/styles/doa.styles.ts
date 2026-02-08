29a42765e0e6a7a278f791380991b18fec189f7b
Codenames-Ren <renmizuki38@gmail.com>
2026-02-08 23:21:33 +0700

    styling doa components

---
+++ src/styles/doa.styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 14,
+    paddingHorizontal: 16,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "#334155",
+  },
+  number: {
+    color: "white",
+    fontSize: 16,
+    width: 32,
+    textAlign: "center",
+    marginRight: 12,
+  },
+  middle: {
+    flex: 1,
+  },
+  nama: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 2,
+  },
+  grup: {
+    color: "#94A3B8",
+    fontSize: 12,
+  },
+});
