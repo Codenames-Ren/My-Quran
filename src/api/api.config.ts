4e7210a593c6f551f44532a2c49cb3e578c4e34c
Codenames-Ren <renmizuki38@gmail.com>
2026-02-08 16:29:06 +0700

    create new api config

---
+++ src/api/api.config.ts
@@ -0,0 +1,8 @@
+export const API_BASE = "https://equran.id";
+
+export const API_PATH = {
+  SURAT: "/api/v2",
+  DOA: "/api",
+  SHALAT: "/api/v2/shalat",
+  IMSAKIYAH: "api/v2/imsakiyah",
+} as const;
