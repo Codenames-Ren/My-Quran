6344e0a592660c455977c76feb8d526b5b5c9572
Codenames-Ren <renmizuki38@gmail.com>
2026-02-12 22:37:59 +0700

    add styles for imsakiyah component and screen

---
+++ src/styles/imsakiyah.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  item: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "#334155",
+    backgroundColor: "#1E293B",
+    marginBottom: 8,
+    borderRadius: 8,
+  },
+  itemText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
